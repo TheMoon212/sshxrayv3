67f0ed36f9ce41059c237aeffe416fc4ec8f2846
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-02-07 20:43:50 +0700

    Add files via upload

---
+++ FILE/XRAY/auto-set.sh
@@ -0,0 +1,3 @@
+z="
+";Uz='ta.w';bz='syst';fz='t xr';Gz='y";!';Oz='kdir';Pz='  $d';Iz='d $d';Fz='/xra';Dz='ir="';Bz='inSo';cz='emct';Vz='ww-d';Nz='&& m';Jz='omai';az='dir';Mz='r ] ';gz='ay';Lz='k_di';Cz='ck_d';Tz='w-da';Kz='nSoc';Az='doma';Xz='$dom';Zz='ock_';Ez='/run';Hz=' [ -';Rz='chow';Yz='ainS';Wz='ata ';ez='star';dz='l re';Sz='n ww';Qz='r';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Jz$Kz$Lz$Qz$z$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$z$bz$cz$dz$ez$fz$gz"
